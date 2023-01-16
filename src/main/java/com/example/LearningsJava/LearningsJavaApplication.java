f03e6b703be132b57d5399a7519e2e33e0ea17bb
lokeshthathuru <sailokesh776@gmail.com>
2023-01-16 23:12:49 +0530

    added scheduling with dependency of schedlock

--- src/main/java/com/example/LearningsJava/LearningsJavaApplication.java
+++ src/main/java/com/example/LearningsJava/LearningsJavaApplication.java
@@ -1,5 +1,6 @@
 package com.example.LearningsJava;
 
+import net.javacrumbs.shedlock.core.SchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,11 +16,20 @@ public class LearningsJavaApplication {
         SpringApplication.run(LearningsJavaApplication.class, args);
     }
 
+//	/**
+//	 *  scheduled time every 6 sec it will run
+//	 */
+//	@Scheduled(fixedDelay = 6000)
+//    public void cronRun() {
+//        System.out.println("Inside cron run:" + new Date());
+//    }
 	/**
-	 *  scheduled time every 6 sec it will run
+	 *  scheduled time every 30 sec it will run
 	 */
-	@Scheduled(fixedDelay = 6000)
-    public void cronRun() {
-        System.out.println("Inside cron run:" + new Date());
-    }
+	@Scheduled(fixedDelay = 30000)
+	@SchedulerLock(name="scheduletest")
+	public void cronRun() throws Exception{
+
+		System.out.println("Inside cron run:" + new Date());
+	}
 }
