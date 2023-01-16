4fc7722b3f47b7662921bc502168d6add2cdf838
lokeshthathuru <sailokesh776@gmail.com>
2023-01-16 21:04:56 +0530

    Initial commit

---
+++ src/main/java/com/example/LearningsJava/LearningsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearningsJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearningsJava/LearningsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearningsJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
