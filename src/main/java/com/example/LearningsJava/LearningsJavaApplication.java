8221da249ec5a798fe56dfc5ac0d88cc61960f8e
lokeshthathuru <sailokesh776@gmail.com>
2023-01-16 22:34:07 +0530

    scheduling in spring boot app

--- src/main/java/com/example/LearningsJava/LearningsJavaApplication.java
+++ src/main/java/com/example/LearningsJava/LearningsJavaApplication.java
@@ -2,12 +2,24 @@ package com.example.LearningsJava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
 
 @SpringBootApplication
+@EnableScheduling
 public class LearningsJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningsJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearningsJavaApplication.class, args);
+    }
 
+	/**
+	 *  scheduled time every 6 sec it will run
+	 */
+	@Scheduled(fixedDelay = 6000)
+    public void cronRun() {
+        System.out.println("Inside cron run:" + new Date());
+    }
 }
